a368e926f5b8bda071cfd0dec4b6b15dfdc685db
abhinavbasu99 <abhinavbasu99@gmail.com>
2023-08-02 17:45:31 +0530

    Added Clear List Functionality

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) {
+      setItems([]);
+    }
+  }
+
   return (
     <>
       <Logo />
@@ -27,6 +37,7 @@ function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onPackedToggle={handlePackedToggle}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </>
@@ -77,11 +88,29 @@ function Form({ onAddItem }) {
   );
 }
 
-function PackingList({ items, onDeleteItem, onPackedToggle }) {
+function PackingList({ items, onDeleteItem, onPackedToggle, onClearList }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -90,6 +119,15 @@ function PackingList({ items, onDeleteItem, onPackedToggle }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
     </div>
   );
 }
